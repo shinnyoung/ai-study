74e587c8f5cc227827f629c2ff903a20478fd66f
shinyoung park <shinyoung8318@gmail.com>
2024-07-07 20:57:43 +0900

    first first commit

---
+++ src/test/java/com/example/aistudy/AiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
