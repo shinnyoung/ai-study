fd3053cd7e502dc4a29dc15fc0941d5a37494d0c
shinyoung park <shinyoung8318@gmail.com>
2024-07-07 20:23:32 +0900

    first commit

--- ai-study/src/main/java/com/example/aistudy/AiStudyApplication.java
+++ ai-study/src/main/java/com/example/aistudy/AiStudyApplication.java
@@ -1,13 +1,18 @@
 package com.example.aistudy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiStudyApplication {
+public class AiStudyApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AiStudyApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World !");
+    }
 }
