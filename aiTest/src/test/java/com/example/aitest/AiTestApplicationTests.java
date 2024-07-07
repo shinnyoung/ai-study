10f46ed4f28021d8648fa5c897108d80a3673b1c
shinyoung park <shinyoung8318@gmail.com>
2024-07-07 19:53:42 +0900

    first commit

---
+++ aiTest/src/test/java/com/example/aitest/AiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
