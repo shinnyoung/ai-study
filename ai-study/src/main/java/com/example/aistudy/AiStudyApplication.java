3c7364c149d0e2a7148dd559fe89e42f22be98ea
shinyoung park <shinyoung8318@gmail.com>
2024-07-07 20:08:52 +0900

    first commit

---
+++ ai-study/src/main/java/com/example/aistudy/AiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.aistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiStudyApplication.class, args);
+    }
+
+}
--- ai-study/src/test/java/com/example/aistudy/AiStudyApplicationTests.java
+++ ai-study/src/test/java/com/example/aistudy/AiStudyApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.aitest;
+package com.example.aistudy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AiTestApplicationTests {
+class AiStudyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
